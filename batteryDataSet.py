a3bcb93dc47f9f8f8309607684a35ed8496cfeb7
Seamus <obersd@utexas.edu>
2021-12-27 12:11:38 -0500

    added comments and test time data field entry

--- batteryDataSet.py
+++ batteryDataSet.py
@@ -12,11 +12,13 @@ class batteryDataSet:
  
         self.cyclenumbers = np.array(data_header_dictionary.get('Cycle_Index')) if sysFormat == 'Arbin' else np.array(data_header_dictionary.get('Cycle-Index2'))
         self.speCapData = np.array(data_header_dictionary.get('Discharge_Capacity(Ah)')) if sysFormat == 'Arbin' else np.array(data_header_dictionary.get('SpeCap/mAh/g2'))
-        self.voltageData = voltageData #Arbin: 'Voltage(V)' | Land: 'Voltage/V'
-        self.step_index_record = step_index #Arbin:'Step_Index' | Land: 'Step_Index'
-        self.cycle_index_record = cycle_index_record #Arbin: 'Cycle_Index' | Land: 'Cycle-Index'
-        #self.charge_capacity_stats = charge_capacity_stats #Arbin: 'Charge_Capacity(Ah)' | Land: 'SpeCap/mAh/g'
-        #self.discharge_capacity_stats = discharge_capacity_stats #Arbin: 'Discharge_Capacity(Ah)' | Land: 'SpeCap/mAh/g'
+        self.voltageData =  #Arbin: 'Voltage(V)' | Land: 'Voltage/V2'
+        self.step_index_record = #Arbin:'Step_Index' | Land: 'Step_Index2'
+        self.cycle_index_record = #Arbin: 'Cycle_Index' | Land: 'Cycle-Index2'
+        self.test_time_record = #Arbin: 'Test_Time(s)' | Land: 'TestTime2'
+         
+        #self.charge_capacity_stats = charge_capacity_stats #Arbin: 'Charge_Capacity(Ah)' | Land: 'SpeCapC/mAh/g'
+        #self.discharge_capacity_stats = discharge_capacity_stats #Arbin: 'Discharge_Capacity(Ah)' | Land: 'SpeCapD/mAh/g'
         #self.charge_voltage_stats = charge_voltage_stats #Arbin: 'Voltage(V)' | Land: 'MedVoltC/V'
         #self.discharge_voltage_stats = discharge_voltage_stats #Arbin: 'Voltage' | Land: 'MedVoltD/V'
         #self.efficency_stats = efficiency_stats #Land: 'Efficiency/%'
